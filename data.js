7cda11fd72abe5342da0e53ac6dc4b1ac8501c15
Luke Liu <buptlxy@163.com>
2020-10-19 01:25:45 +0800

    release-version

--- data.js
+++ data.js
@@ -2,31 +2,31 @@ var data = [{
         name: "张三", //名字
         tel: "18812345678", //电话
         email: "102@qq.com", //邮箱
-        address: "王附井", //地址
+        address: "西土城路10号", //地址
         qq: "1025962522", //qq
         id:1
     },
     {
-        name: "张三",
+        name: "王五",
         tel: "18812345678",
         email: "102@qq.com",
-        address: "王附井",
+        address: "北京邮电大学",
         qq: "1025962522",
         id:2
     },
     {
-        name: "张三",
+        name: "李四",
         tel: "18812345678",
         email: "102@qq.com",
-        address: "王附井",
+        address: "北京邮电大学沙河校区",
         qq: "1025962522",
         id:3
     },
     {
-        name: "张三",
-        tel: "18812345678",
+        name: "John",
+        tel: "13589871004",
         email: "102@qq.com",
-        address: "王附井",
+        address: "北京师范大学",
         qq: "1025962522",
         id:4
     },
